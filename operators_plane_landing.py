6abcc3c0c3ef2b6cf2a00957a63da13a3aeef5a1
Ni4ka1991 <astahova_nica@mail.ru>
2021-03-09 12:13:47 +0200

    Add file operators_plane_landing.py

---
+++ operators_plane_landing.py
@@ -0,0 +1,27 @@
+#1/usr/bin/env python3
+
+#data
+
+plane_altitude_max = 500 #meters. Maximal for landing. 
+plane_altitude_min = 100 #meters. Minimal for landing. 
+
+plane_speed_max = 300 #km/hours
+plane_speed_min = 200 #km/hours
+
+wind_speed = 100 #km/hours
+fair_wind = True
+
+fuel_volume_min = 1 #perсent
+
+technical_difficulites = True
+
+
+#logic
+
+#can_land = plane_altitude < 700 and plane_speed < 500
+
+
+
+#view
+
+#print("Can the plane land?", can_land)
